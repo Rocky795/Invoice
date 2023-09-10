298e3005029d8338f14faaff471b060ea66bd088
Rocky <createcreate313@gmail.com>
2023-09-11 00:05:15 +0530

    adding allowed host without http://

--- Invoice/settings.py
+++ Invoice/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-0)poe$$t#q2(86s1)i@7q9k1h&ap7$mz=e_sx_1x(7f7fx2$xl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://invoice-03sj.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['invoice-03sj.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
