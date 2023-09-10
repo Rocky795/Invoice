5b176522dececd7f1117592dbc3183161392ee65
Rocky <createcreate313@gmail.com>
2023-09-10 22:48:24 +0530

    Push after adding .gitignour

--- Invoice/settings.py
+++ Invoice/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import dj_database_url
+import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -127,6 +128,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [BASE_DIR/'static/',]
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles_build','static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
